db0c723de2ab3fb76c95bf620e1dc023842d69a2
farkmu45 <farukmicros@outlook.com>
2021-11-28 16:56:36 +0700

    Delete useless comment

--- app/src/test/java/com/tigro/lajoie/AuthUnitTest.kt
+++ app/src/test/java/com/tigro/lajoie/AuthUnitTest.kt
@@ -11,11 +11,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
 @ExperimentalCoroutinesApi
 class AuthUnitTest {
 
