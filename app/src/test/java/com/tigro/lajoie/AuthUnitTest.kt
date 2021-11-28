d441e8cbb19a0ca1e2115d7cd73d5b7c5f86d855
farkmu45 <farukmicros@outlook.com>
2021-11-28 20:34:25 +0700

    Change test from viewmodel method to retrofit calls

--- app/src/test/java/com/tigro/lajoie/AuthUnitTest.kt
+++ app/src/test/java/com/tigro/lajoie/AuthUnitTest.kt
@@ -1,13 +1,16 @@
 package com.tigro.lajoie
 
-import com.tigro.lajoie.screens.auth.ApiStatus
-import com.tigro.lajoie.screens.auth.AuthViewModel
+import com.tigro.lajoie.models.AuthToken
+import com.tigro.lajoie.network.LajoieApi
+import com.tigro.lajoie.network.LoginBody
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.After
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
@@ -15,7 +18,6 @@ import org.junit.Test
 @ExperimentalCoroutinesApi
 class AuthUnitTest {
 
-    private val viewModel = AuthViewModel()
     private val dispatcher = StandardTestDispatcher()
 
     @Before
@@ -29,74 +31,58 @@ class AuthUnitTest {
     }
 
     @Test
-    fun `logging in using empty data will set the status to ERROR`() = runTest {
-        TestScope().launch {
-            viewModel.apply {
-                email.value = ""
-                password.value = ""
-                login()
-            }
-            assertEquals(ApiStatus.FAILED, viewModel.status.value)
+    fun `logging in using empty data will throw Exception`() = runBlocking {
+        try {
+            LajoieApi.retrofitService.login(LoginBody("", ""))
+            assertFalse(true)
+        } catch (e: Exception) {
+            assertTrue(true)
         }
     }
 
     @Test
-    fun `logging in using empty password will set the status to ERROR`() = runTest {
-        TestScope().launch {
-            viewModel.apply {
-                email.value = "fark@gmail.com"
-                password.value = ""
-                login()
-            }
-            assertEquals(ApiStatus.FAILED, viewModel.status.value)
+    fun `logging in using empty password will throw Exception`() = runBlocking {
+        try {
+            LajoieApi.retrofitService.login(LoginBody("fark@gmail.com", ""))
+            assertFalse(true)
+        } catch (e: Exception) {
+            assertTrue(true)
         }
     }
 
     @Test
-    fun `logging in using invalid password will set the status to ERROR`() = runTest {
-        TestScope().launch {
-            viewModel.apply {
-                email.value = "fark@gmail.com"
-                password.value = "dfsafds"
-                login()
-            }
-            assertEquals(ApiStatus.FAILED, viewModel.status.value)
+    fun `logging in using invalid password will throw Exception`() = runBlocking {
+        try {
+            LajoieApi.retrofitService.login(LoginBody("fark@gmail.com", "maulsdfas"))
+            assertFalse(true)
+        } catch (e: Exception) {
+            assertTrue(true)
         }
     }
 
     @Test
-    fun `logging in using empty email will set the status to ERROR`() = runTest {
-        TestScope().launch {
-            viewModel.apply {
-                email.value = ""
-                password.value = "maulana123"
-                login()
-            }
-            assertEquals(ApiStatus.FAILED, viewModel.status.value)
+    fun `logging in using empty email will throw Exception`() = runBlocking {
+        try {
+            LajoieApi.retrofitService.login(LoginBody("", "maulana123"))
+            assertFalse(true)
+        } catch (e: Exception) {
+            assertTrue(true)
         }
     }
 
     @Test
-    fun `logging in using invalid email will set the status to ERROR`() = runTest {
-        TestScope().launch {
-            viewModel.apply {
-                email.value = "faek@gmail.com"
-                password.value = "maulana123"
-                login()
-            }
-            assertEquals(ApiStatus.FAILED, viewModel.status.value)
+    fun `logging in using invalid email will throw Exception`() = runBlocking {
+        try {
+            LajoieApi.retrofitService.login(LoginBody("farko@gmail.com", "maulana123"))
+            assertFalse(true)
+        } catch (e: Exception) {
+            assertTrue(true)
         }
     }
 
     @Test
-    fun `logging in using valid credentials will set the status to SUCCESS`() = runTest {
-        TestScope().launch {
-            viewModel.apply {
-                email.value = "faek@gmail.com"
-                password.value = "maulana123"
-                login()
-            }
-            assertEquals(ApiStatus.SUCCESS, viewModel.status.value)
-        }
+    fun `logging in using valid credentials will send the token`() = runBlocking {
+        val token = LajoieApi.retrofitService.login(LoginBody("fark@gmail.com", "maulana123"))
+        assertEquals(AuthToken("ZmFya0BnbWFpbC5jb206bWF1bGFuYTEyMw=="), token)
     }
 }
