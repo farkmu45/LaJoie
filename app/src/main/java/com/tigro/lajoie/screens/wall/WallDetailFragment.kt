dd9980127d1a4bca60e438015e0c40bcf0462db1
farkmu45 <farukmicros@outlook.com>
2022-01-08 17:51:23 +0700

    Clear unused import

--- app/src/main/java/com/tigro/lajoie/screens/wall/WallDetailFragment.kt
+++ app/src/main/java/com/tigro/lajoie/screens/wall/WallDetailFragment.kt
@@ -1,7 +1,6 @@
 package com.tigro.lajoie.screens.wall
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
